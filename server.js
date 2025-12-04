028d0f84b74b9a384e6d416931421a322207448e
truechu <cu10763010@sju.edu>
2025-12-04 11:11:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static("public"));
 
 // LOGIN
